6395af6d841f534979d346813498c73ce7e85292
cromueloliver02 <cromuelbarut@gmail.com>
2022-12-07 21:30:18 +0800

    clean up

---
+++ lib/grocery_app.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class GroceryApp extends StatelessWidget {
+  const GroceryApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Grocery Mobile',
+      theme: ThemeData.light(),
+      home: const Scaffold(
+        body: Center(
+          child: Text('GROCERY MOBILE APP'),
+        ),
+      ),
+    );
+  }
+}
