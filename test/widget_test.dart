d6a81e365dec17fef4cb52f930772ab62b7aaf45
cromueloliver02 <cromuelbarut@gmail.com>
2022-12-15 14:30:23 +0800

    saved unsaved changes

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:grocery_mobile/grocery_app.dart';
+import 'package:grocery_mobile/presentation/grocery_app.dart';
 import 'package:grocery_mobile/presentation/utils/utils.dart';
 
 void main() {
