15e70f33e76209e98f129951cf69681a4470985f
cromueloliver02 <cromuelbarut@gmail.com>
2022-12-17 23:40:12 +0800

    created badge for the count of products in cart icon

--- lib/presentation/pages/navigation/components/navigation_page_bottom_bar.dart
+++ lib/presentation/pages/navigation/components/navigation_page_bottom_bar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:badges/badges.dart';
 import 'package:flutter_iconly/flutter_iconly.dart';
 
 import '../../../../business_logic/cubits/cubits.dart';
@@ -20,23 +21,47 @@ class NavigationPageBottomBar extends StatelessWidget {
         showUnselectedLabels: false,
         currentIndex: state.currentIndex,
         onTap: (index) => _onChangeIndex(context, index),
-        items: const [
-          BottomNavigationBarItem(
+        items: [
+          const BottomNavigationBarItem(
             label: 'Home',
             icon: Icon(IconlyLight.home),
             activeIcon: Icon(IconlyBold.home),
           ),
-          BottomNavigationBarItem(
+          const BottomNavigationBarItem(
             label: 'Category',
             icon: Icon(IconlyLight.category),
             activeIcon: Icon(IconlyBold.category),
           ),
           BottomNavigationBarItem(
             label: 'Cart',
-            icon: Icon(IconlyLight.buy),
-            activeIcon: Icon(IconlyBold.buy),
+            icon: Badge(
+              toAnimate: true,
+              shape: BadgeShape.circle,
+              badgeColor: Colors.blue,
+              borderRadius: BorderRadius.circular(8),
+              position: BadgePosition.topEnd(top: -7, end: -7),
+              badgeContent: const Text(
+                '0',
+                style: TextStyle(color: Colors.white),
+              ),
+              child: const Icon(IconlyLight.buy),
+            ),
+            activeIcon: Badge(
+              toAnimate: true,
+              shape: BadgeShape.circle,
+              badgeColor: Colors.blue,
+              borderRadius: BorderRadius.circular(8),
+              position: BadgePosition.topEnd(top: -7, end: -7),
+              badgeContent: const FittedBox(
+                child: Text(
+                  '0',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+              child: const Icon(IconlyBold.buy),
+            ),
           ),
-          BottomNavigationBarItem(
+          const BottomNavigationBarItem(
             label: 'User',
             icon: Icon(IconlyLight.user2),
             activeIcon: Icon(IconlyBold.user2),
